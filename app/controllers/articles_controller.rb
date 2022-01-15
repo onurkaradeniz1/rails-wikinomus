530dbc379932726cdbda4343b634a1d22b95022c
Onur Karadeniz <onurkaradeniz1@gmail.com>
2022-01-15 16:46:17 +0100

    new and index are completed will finish the rest later

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @articles = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
